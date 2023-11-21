50eb1cab73f58e300df902f5cc4259eb2311d3f8
Manel <maneldelahozrodriguez@gmail.com>
2023-11-21 20:32:38 +0100

    First Commit

---
+++ exercise_09/index.js
@@ -0,0 +1,7 @@
+import getDistanceMessageFromSumTo100 from "./getDistanceMessageFromSumTo100.js";
+
+const result1 = getDistanceMessageFromSumTo100(70);
+console.log(result1);
+
+const result2 = getDistanceMessageFromSumTo100(30);
+console.log(result2);
